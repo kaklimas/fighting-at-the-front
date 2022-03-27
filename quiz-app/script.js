bc2e944770b2ef87e25891a591c33ac30ad9ae62
kaklimas <kaklimas@student.agh.edu.pl>
2022-03-27 20:11:25 +0200

    quiz init

---
+++ quiz-app/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old am I?',
+        a: '20',
+        b: '9',
+        c: '2',
+        d: '5',
+        correct: 'a'
+    }, {
+        question: 'Best programming language?',
+        a: 'Java',
+        b: 'Python',
+        c: 'C',
+        d: 'JavaScript',
+        correct: 'a'
+    }, {
+        question: 'Question 3',
+        a: 'Answear 1',
+        b: 'Answear 2',
+        c: 'Answear 3',
+        d: 'Answear 4',
+        correct: 'c'
+    }, {
+        question: 'Question 4',
+        a: 'Answear 1',
+        b: 'Answear 2',
+        c: 'Answear 3',
+        d: 'Answear 4',
+        correct: 'b'
+    }, {
+        question: 'Question 5',
+        a: 'Answear 1',
+        b: 'Answear 2',
+        c: 'Answear 3',
+        d: 'Answear 4',
+        correct: 'd'
+    }
+]
