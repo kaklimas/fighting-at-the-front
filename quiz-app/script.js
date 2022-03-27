5887e26b71fce746a8e4ee33c8fbecef424dce36
kaklimas <kaklimas@student.agh.edu.pl>
2022-03-27 21:15:12 +0200

    done

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -36,3 +36,69 @@ const quizData = [
         correct: 'd'
     }
 ]
+
+const quiz = document.getElementById('quiz');  
+const answersEl = document.querySelectorAll(".answer");
+
+
+const a_text = document.getElementById('a_text');  
+const b_text = document.getElementById('b_text'); 
+const c_text = document.getElementById('c_text'); 
+const d_text = document.getElementById('d_text'); 
+
+const submitBtn = document.getElementById('submit');
+const questionEl = document.getElementById('question');
+
+let currentQuestion = 0;
+let score = 0;
+
+loadQuiz();
+
+function getSelected() {
+    const answersEl = document.querySelectorAll(".answer");
+    let answer = undefined;
+    answersEl.forEach((answerEl) => {
+        if (answerEl.checked){
+            answer =  answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function loadQuiz() {
+    deselectAnswer();
+    const currentQuizData = quizData[currentQuestion];
+    questionEl.innerHTML = currentQuizData.question;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+}
+
+function deselectAnswer(){
+    answersEl.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', ()=>{
+    const answer = getSelected();
+    
+    if (answer){
+        if (answer == quizData[currentQuestion].correct){
+            score++;
+        }
+        currentQuestion++;
+        if (currentQuestion < quizData.length){
+            loadQuiz();
+        } else {
+            quiz.innerHTML = 
+                `<h2>
+                    You answered correctly at ${score}/
+                    ${quizData.length} questions.
+                </h2>
+                <button onClick="location.reload()">Reload</button>`;
+        }    
+    }
+
+})
